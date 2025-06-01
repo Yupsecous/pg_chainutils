ff2d652f8e3c9d12e35506a24b344c8093127146
Yupsecous <zxweb8895@gmail.com>
2025-06-01 16:03:14 +0200

    u256: make parse big endian

--- src/u256.rs
+++ src/u256.rs
@@ -3,19 +3,44 @@ use pgrx::prelude::*;
 #[pg_schema]
 #[allow(non_snake_case)]
 mod U256 {
+    use alloy::core::hex;
     use alloy::core::primitives::U256;
 
     use pgrx::prelude::*;
 
     #[pg_extern(name = "parse", immutable, parallel_safe)]
-    fn parse_u256(string: &str) -> pgrx::AnyNumeric {
+    fn parse(string: &str) -> pgrx::AnyNumeric {
         pgrx::AnyNumeric::try_from(
-            string
-                .parse::<U256>()
-                .expect("string is U256")
+            U256::from_be_slice(&hex::decode(string).expect("to be big endian hex"))
                 .to_string()
                 .as_str(),
         )
-        .expect("can be converted to numeric")
+        .expect("Failed to convert U256 to AnyNumeric")
+    }
+}
+
+#[cfg(any(test, feature = "pg_test"))]
+#[pg_schema]
+mod tests {
+    use pgrx::datum::DatumWithOid;
+    use pgrx::prelude::*;
+
+    use anyhow::Result;
+    use std::str::FromStr;
+
+    #[pg_test]
+    fn u256_parse() -> Result<()> {
+        let data = "0000000000000000000000000000000000000000000024be4b7d04405304e8de";
+
+        let parsed = Spi::get_one_with_args::<pgrx::AnyNumeric>(
+            "SELECT U256.parse($1);",
+            &vec![DatumWithOid::from(data)],
+        )?;
+
+        assert_eq!(
+            parsed,
+            Some(pgrx::AnyNumeric::from_str("173515514265911293176030")?)
+        );
+        Ok(())
     }
 }
